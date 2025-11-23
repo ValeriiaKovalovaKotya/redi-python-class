bf6ebd23d336cb74801f2ed6fda4ab6795ca7402
Valeriia <valeriiakovalovakotya@gmail.com>
2025-11-23 21:58:34 +0100

    rm-lesson-2-homework.py

--- lesson-2-homework.py
+++
@@ -1,9 +0,0 @@
-while True:
-    user_input = input("Enter a string: ")
-    if user_input == "":
-        print("Thanks for playing!")
-        break
-    print("First letter capitalized:\t".expandtabs(1) + user_input.capitalize())
-    print("All lowercase:\t".expandtabs(13) + user_input.lower())
-    print("Title case:\t".expandtabs(26) + user_input.title())
-    print("All uppercase:\t".expandtabs(13) + user_input.upper())
